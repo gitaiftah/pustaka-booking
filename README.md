d4dbe98fea9cdc5b6fe8ef58bdd129c3d6d52cc5
gitaiftah <ruswadisumardi@gmail.com>
2022-10-28 11:11:15 +0700

    upload pembaruan

--- README.md
+++
@@ -1,4 +0,0 @@
-U (Untracked) = File yang belum pernah dimasukkan ke dalam Git (file baru)
-M (Modified) = File yang udah masuk ke dalam databae Git dan udah pernah dimodifikasi
-D (Deleted) = File yang dihapus
-A (Added) = File yang ditambahkan kedalam mode staged
---
+++ application/views/view-latihan.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Penjumlahan</title>
+</head>
+<body>
+    Saat ini sedang belajar web programming <br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    Ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
