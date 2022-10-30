ea8c7fc2f6574183ec0dfca7dc7a06c006e8b0f7
gitaiftah <ruswadisumardi@gmail.com>
2022-10-30 16:34:01 +0700

    Menambah data siswa

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,8 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-// $autoload['libraries'] = array('form_validation','session','database');
-$autoload['libraries'] = array('database');
+$autoload['libraries'] = array('database','form_validation','session');
 
 /*
 | -------------------------------------------------------------------
@@ -90,8 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-// $autoload['helper'] = array('url');
-$autoload['helper'] = array('url','form');
+$autoload['helper'] = array('url');
 
 /*
 | -------------------------------------------------------------------
@@ -134,5 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-// $autoload['model'] = array('ModelUser', 'ModelBuku');
-$autoload['model'] = array('ModelSiswa');
+$autoload['model'] = array('ModelUser', 'ModelBuku', 'ModelSiswa');
