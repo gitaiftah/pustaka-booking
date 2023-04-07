c77588e9499a49ce63e86b5ea70dda50af9f6163
gitaiftah <ruswadisumardi@gmail.com>
2023-04-07 19:35:16 +0700

    pertemuan3

--- application/views/buku/kategori.php
+++ application/views/buku/kategori.php
@@ -28,8 +28,8 @@
                               <th scope="row"><?= $a++; ?></th>
                               <td><?= $k['kategori']; ?></td>
                               <td>
-                                   <a href="<?=base_url('buku/ubahBuku/').$k['id'];?>" class="badge badge-info"><i class="fas fa-edit"></i> Ubah</a>
-                                   <a href="<?=base_url('buku/hapusbuku/').$k['id'];?>" onclick="return confirm('Kamu yakin akan menghapus <?= $judul.' '.$k['kategori'];?>?');" class="badge badge-danger"><i class="fas fa-trash"></i>Hapus</a>
+                                   <a href="<?=base_url('buku/ubahKategori/').$k['id'];?>" class="badge badge-info"><i class="fas fa-edit"></i> Ubah</a>
+                                   <a href="<?=base_url('buku/hapuskategori/').$k['id'];?>" onclick="return confirm('Kamu yakin akan menghapus <?= $judul.' '.$k['kategori'];?>?');" class="badge badge-danger"><i class="fas fa-trash"></i>Hapus</a>
                               </td>
                          </tr>
                          <?php } ?>
@@ -45,30 +45,27 @@
 <!-- End of Main Content -->
 
 <!-- Modal Tambah kategori baru-->
-<div class="modal fade" id="kategoriBaruModal" tabindex="-1"role="dialog" aria-labelledby="kategoriBaruModalLabel" aria-hidden="true">
-     <div class="modal-dialog" role="document">
-          <div class="modal-content">
-               <div class="modal-header">
-                    <h5 class="modal-title"id="kategoriBaruModalLabel">Tambah Kategori</h5>
-<button type="button" class="close" data-
-dismiss="modal" aria-label="Close">
-<span aria-hidden="true">&times;</span>
-</button>
+<div class="modal fade" id="kategoriBaruModal" tabindex="-1" role="dialog" aria-labelledby="kategoriBaruModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="kategoriBaruModalLabel">Tambah Kategori</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="<?= base_url('buku/kategori'); ?>" method="post">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <input type="text" name="kategori" id="kategori" placeholder="Masukkan Nama Kategori" class="form-control form-control-user">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal"><i class="fas fa-ban"></i> Close</button>
+                    <button type="submit" class="btn btn-primary"><i class="fas fa-plus-circle"></i> Tambah</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
-<form action="<?= base_url('buku/kategori'); ?>"
-method="post">
-<div class="modal-body">
-<div class="form-group">
-<select name="kategori" class="form-control
-form-control-user">
-<option value="">Pilih Kategori</option>
-<?php
-$k =
-['Sains','Hobby','Komputer','Komunikasi','Hukum','Agama','Populer','
-Bahasa','Komik'];
-for ($i=0;$i<9;$i++) { ?>
-<option value="<?= $k[$i];?>"><?=
-$k[$i];?></option>
-<?php } ?>
-</select>
-</div>
+<!-- End of Modal Tambah Mneu -->
